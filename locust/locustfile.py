002bc5f1a55595a26cc0fdf8eb9306845e6d2961
Martin Koldovsky <mkoldov@gmail.com>
2023-03-02 19:05:40 +0100

    .

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -7,13 +7,13 @@ class WebsiteUser(HttpUser):
     def get_token(self):
         self.client.post("http://localhost:8000/auth/realms/master/protocol/openid-connect/token", {"client_id": "admin-cli", "username": "admin", "password": "admin", "grant_type": "password"}, headers={"Connection": "close"})
 
-    # @task
-    # def get_users(self):
-    #     self.client.adapters.DEFAULT_RETRIES = 5
-    #     r = self.client.post("http://localhost:8080/auth/realms/master/protocol/openid-connect/token", data= {"client_id": "admin-cli", "username": "admin", "password": "admin", "grant_type": "password"}, headers={"Connection": "close"}).text
-    #     print(r)
-    #     h =  {"Authorization": "Bearer "+json.loads(r)["access_token"], "Connection": "close"}
-    #     self.client.get("http://localhost:8080/auth/admin/realms/master/users", headers=h, verify=False)
+    @task
+    def get_users(self):
+        self.client.adapters.DEFAULT_RETRIES = 5
+        r = self.client.post("http://localhost:8000/auth/realms/master/protocol/openid-connect/token", data= {"client_id": "admin-cli", "username": "admin", "password": "admin", "grant_type": "password"}, headers={"Connection": "close"}).text
+        # print(r)
+        h =  {"Authorization": "Bearer "+json.loads(r)["access_token"], "Connection": "close"}
+        self.client.get("http://localhost:8000/auth/admin/realms/master/users", headers=h, verify=False)
 
 
 
